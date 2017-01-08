653dacd87d9e9416cf415cde4cd69dea410d0fc2
Vince Delricco <vd12scbc@gmail.com>
2017-01-08 12:17:25 -0700

    Refactored TwitterActivity by removing deprecated Apache HTTP code in favor of Android Volley.

--- app/src/main/java/com/delricco/vince/revolardemo/TwitterActivity.java
+++ app/src/main/java/com/delricco/vince/revolardemo/TwitterActivity.java
@@ -4,38 +4,47 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.widget.ArrayAdapter;
-import com.google.gson.Gson;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
 
-import java.io.*;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * All Twitter code adapted from: https://github.com/Rockncoder/TwitterTutorial
  */
 public class TwitterActivity extends ListActivity {
 
+    final static String TAG = TwitterActivity.class.getSimpleName();
+
     private ListActivity activity;
-    final static String ScreenName = "Revolar";
-    final static String LOG_TAG = TwitterActivity.class.getSimpleName();
+    private ArrayAdapter<Tweet> adapter;
+    private Twitter twits;
+    private RequestQueue requestQueue;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activity = this;
 
+        requestQueue = Volley.newRequestQueue(this);
+        twits = new Twitter();
+        adapter = new ArrayAdapter<>(activity, android.R.layout.simple_list_item_1, twits);
+        setListAdapter(adapter);
         downloadTweets();
     }
 
@@ -45,140 +54,139 @@ public class TwitterActivity extends ListActivity {
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
         if (networkInfo != null && networkInfo.isConnected()) {
-            new DownloadTwitterTask().execute(ScreenName);
+            /* Begins the process of grabbing Twitter timeline. Next steps are handled
+               by Volley response listeners */
+            getAuthToken();
         } else {
-            Log.v(LOG_TAG, "No network connection available.");
+            Log.v(TAG, "No network connection available.");
         }
     }
 
-    // Uses an AsyncTask to download a Twitter user's timeline
-    private class DownloadTwitterTask extends AsyncTask<String, Void, String> {
-        final static String CONSUMER_KEY = "aA23JBYz0xrKrWoYNLKd0oiKI";
-        final static String CONSUMER_SECRET = "cujxceo92OIoGQDRCVLXDJ9GXkG2c8XLxbwRZAvCxZBdCxed9u";
-        final static String TwitterTokenURL = "https://api.twitter.com/oauth2/token";
-        final static String TwitterStreamURL = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=";
+    private Twitter jsonToTwitter(String result) {
+        Twitter twits = null;
+        if (result != null && result.length() > 0) {
+            try {
+                twits = new Gson().fromJson(result, Twitter.class);
+            } catch (IllegalStateException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return twits;
+    }
+
+    private Authenticated jsonToAuthenticated(String rawAuthorization) {
+        Authenticated auth = null;
+        if (rawAuthorization != null && rawAuthorization.length() > 0) {
+            try {
+                auth = new Gson().fromJson(rawAuthorization, Authenticated.class);
+            } catch (IllegalStateException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return auth;
+    }
+
+    private void getAuthToken() {
+        // Step 1: Encode consumer key and secret
+        try {
+            // URL encode the consumer key and secret
+            String urlApiKey = URLEncoder.encode(getString(R.string.twitter_consumer_key), "UTF-8");
+            String urlApiSecret = URLEncoder.encode(getString(R.string.twitter_consumer_secret), "UTF-8");
+
+            // Concatenate the encoded consumer key, a colon character, and the
+            // encoded consumer secret
+            final String combined = urlApiKey + ":" + urlApiSecret;
+
+            // Base64 encode the string
+            final String base64Encoded = Base64.encodeToString(combined.getBytes(), Base64.NO_WRAP);
+
+            Request authTokenRequest = new AuthTokenRequest(
+                    getString(R.string.twitter_auth_token_url),
+                    new AuthResponseListener(),
+                    new GenericErrorListener(),
+                    base64Encoded);
+            requestQueue.add(authTokenRequest);
+        } catch (UnsupportedEncodingException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private class AuthTokenRequest extends StringRequest {
+        private String encodedKeyAndSecret;
+
+        private AuthTokenRequest(String url,
+                                 Response.Listener<String> listener,
+                                 Response.ErrorListener errorListener,
+                                 String encodedKeyAndSecret)
+        {
+            super(Request.Method.POST, url, listener, errorListener);
+            this.encodedKeyAndSecret = encodedKeyAndSecret;
+        }
 
         @Override
-        protected String doInBackground(String... screenNames) {
-            String result = null;
-
-            if (screenNames.length > 0) {
-                result = getTwitterStream(screenNames[0]);
-            }
-            return result;
+        public Map getHeaders() throws AuthFailureError {
+            Map headers = new HashMap();
+            headers.put("Content-Length", String.valueOf(getBody().length));
+            headers.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            headers.put("Authorization", "Basic " + encodedKeyAndSecret);
+            return headers;
         }
 
-        // onPostExecute convert the JSON results into a Twitter object (which is an Array list of tweets)
         @Override
-        protected void onPostExecute(String result) {
-            Twitter twits = jsonToTwitter(result);
+        public byte[] getBody() {
+            return ("grant_type=client_credentials").getBytes();
+        }
+    }
 
-            // lets write the results to the console as well
-            for (Tweet tweet : twits) {
-                Log.i(LOG_TAG, tweet.getText());
-            }
+    private class TwitterTimelineRequest extends StringRequest {
+        private Authenticated auth;
 
-            // send the tweets to the adapter for rendering
-            ArrayAdapter<Tweet> adapter = new ArrayAdapter<Tweet>(activity, android.R.layout.simple_list_item_1, twits);
-            setListAdapter(adapter);
+        private TwitterTimelineRequest(String url,
+                                       Response.Listener<String> listener,
+                                       Response.ErrorListener errorListener,
+                                       Authenticated auth) {
+            super(Method.GET, url, listener, errorListener);
+            this.auth = auth;
         }
 
-        // converts a string of JSON data into a Twitter object
-        private Twitter jsonToTwitter(String result) {
-            Twitter twits = null;
-            if (result != null && result.length() > 0) {
-                try {
-                    twits = new Gson().fromJson(result, Twitter.class);
-                } catch (IllegalStateException ex) {
-                    // just eat the exception
-                }
-            }
-            return twits;
+        @Override
+        public Map getHeaders() throws AuthFailureError {
+            Map headers = new HashMap();
+            headers.put("Authorization", "Bearer " + auth.access_token);
+            headers.put("Content-Type", "application/json");
+            return headers;
         }
+    }
 
-        // convert a JSON authentication object into an Authenticated object
-        private Authenticated jsonToAuthenticated(String rawAuthorization) {
-            Authenticated auth = null;
-            if (rawAuthorization != null && rawAuthorization.length() > 0) {
-                try {
-                    auth = new Gson().fromJson(rawAuthorization, Authenticated.class);
-                } catch (IllegalStateException ex) {
-                    // just eat the exception
-                }
-            }
-            return auth;
+    private class TwitterTimelineResponseListener implements Response.Listener<String> {
+        @Override
+        public void onResponse(String response) {
+            twits.clear();
+            twits.addAll(jsonToTwitter(response));
+            adapter.notifyDataSetChanged();
         }
+    }
 
-        private String getResponseBody(HttpRequestBase request) {
-            StringBuilder sb = new StringBuilder();
-            try {
-
-                DefaultHttpClient httpClient = new DefaultHttpClient(new BasicHttpParams());
-                HttpResponse response = httpClient.execute(request);
-                int statusCode = response.getStatusLine().getStatusCode();
-                String reason = response.getStatusLine().getReasonPhrase();
-
-                if (statusCode == 200) {
-
-                    HttpEntity entity = response.getEntity();
-                    InputStream inputStream = entity.getContent();
-
-                    BufferedReader bReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
-                    String line = null;
-                    while ((line = bReader.readLine()) != null) {
-                        sb.append(line);
-                    }
-                } else {
-                    sb.append(reason);
-                }
-            } catch (UnsupportedEncodingException ex) {
-            } catch (IOException ex2) {
+    private class AuthResponseListener implements Response.Listener<String> {
+        @Override
+        public void onResponse(String response) {
+            final Authenticated auth = jsonToAuthenticated(response);
+            // Applications should verify that the value associated with the
+            // token_type key of the returned object is bearer
+            if (auth != null && auth.token_type.equals("bearer")) {
+                Request twitterTimelineRequest = new TwitterTimelineRequest(
+                        getString(R.string.twitter_timeline_url) + getString(R.string.revolar),
+                        new TwitterTimelineResponseListener(),
+                        new GenericErrorListener(),
+                        auth
+                );
+                requestQueue.add(twitterTimelineRequest);
             }
-            return sb.toString();
         }
+    }
 
-        private String getTwitterStream(String screenName) {
-            String results = null;
-
-            // Step 1: Encode consumer key and secret
-            try {
-                // URL encode the consumer key and secret
-                String urlApiKey = URLEncoder.encode(CONSUMER_KEY, "UTF-8");
-                String urlApiSecret = URLEncoder.encode(CONSUMER_SECRET, "UTF-8");
-
-                // Concatenate the encoded consumer key, a colon character, and the
-                // encoded consumer secret
-                String combined = urlApiKey + ":" + urlApiSecret;
-
-                // Base64 encode the string
-                String base64Encoded = Base64.encodeToString(combined.getBytes(), Base64.NO_WRAP);
-
-                // Step 2: Obtain a bearer token
-                HttpPost httpPost = new HttpPost(TwitterTokenURL);
-                httpPost.setHeader("Authorization", "Basic " + base64Encoded);
-                httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-                httpPost.setEntity(new StringEntity("grant_type=client_credentials"));
-                String rawAuthorization = getResponseBody(httpPost);
-                Authenticated auth = jsonToAuthenticated(rawAuthorization);
-
-                // Applications should verify that the value associated with the
-                // token_type key of the returned object is bearer
-                if (auth != null && auth.token_type.equals("bearer")) {
-
-                    // Step 3: Authenticate API requests with bearer token
-                    HttpGet httpGet = new HttpGet(TwitterStreamURL + screenName);
-
-                    // construct a normal HTTPS request and include an Authorization
-                    // header with the value of Bearer <>
-                    httpGet.setHeader("Authorization", "Bearer " + auth.access_token);
-                    httpGet.setHeader("Content-Type", "application/json");
-                    // update the results with the body of the response
-                    results = getResponseBody(httpGet);
-                }
-            } catch (UnsupportedEncodingException ex) {
-            } catch (IllegalStateException ex1) {
-            }
-            return results;
-        }
+    private class GenericErrorListener implements Response.ErrorListener {
+        @Override
+        public void onErrorResponse(VolleyError error) { Log.e("onErrorResponse", error.toString()); }
     }
 }
